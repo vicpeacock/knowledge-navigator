528fc70f9efb49c1bd4755977cadcec45f197835
Vincenzo Pallotta <vincenzo.pallotta@gmail.com>
2025-11-23 23:49:33 +0100

    Add initial schema migration to create base tables

--- backend/alembic/versions/a39125eacc42_add_title_description_status_and_.py
+++ backend/alembic/versions/a39125eacc42_add_title_description_status_and_.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = 'a39125eacc42'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '000_initial_schema'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
